6900df2d68471a191078be3d4ed30f79e7dd80e2
neutralpronoun <alexowendavies@gmail.com>
2024-09-09 12:12:43 +0100

    Conda seems to have installed local/private pip packages. package-fix installs generic pypi versions, then writes requirements.txt.

---
+++ package-fix.sh
@@ -0,0 +1,10 @@
+# List all packages installed from local paths
+pip freeze | grep '@ file://' | cut -d ' ' -f 1 | while read package; do
+    # Uninstall the local package
+    pip uninstall -y $package
+
+    # Reinstall the package from PyPI (without version constraints)
+    pip install $package
+done
+pip freeze > requirements.txt
+
