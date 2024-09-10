1b22b37d1424a0c190f2bba6cd74e06a9c352cc7
neutralpronoun <alexowendavies@gmail.com>
2024-09-10 22:42:56 +0100

    one more try with package versioning

--- package-fix.sh
+++ package-fix.sh
@@ -3,12 +3,14 @@ pip freeze | grep '@ file://' | cut -d ' ' -f 1 | while read package; do
     # Uninstall the local package
     pip uninstall -y $package
 
-    # Reinstall the package from PyPI (without version constraints)
+    # Reinstall the package from PyPI without version constraints
     pip install $package
 done
 
 # Generate requirements.txt excluding specific packages and remove + tags from versions
-pip freeze | grep -vE 'torch_cluster|torch_sparse|torch_scatter' | sed 's/+[^=]*//g' > requirements.txt
+# Also, remove version pinning entirely by stripping '==' and following characters.
+pip freeze | grep -vE 'torch_cluster|torch_sparse|torch_scatter' | sed 's/==.*//g' | sed 's/+[^=]*//g' > requirements.txt
 
 # Export the environment.yml using conda
-conda env export --from-history | tee environment.yml
+# --no-builds will exclude build strings and loosen package versioning.
+conda env export --from-history --no-builds | tee environment.yml
